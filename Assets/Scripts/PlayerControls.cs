e64863a2af625146d5cbdfb7db112f49ed627644
Jerome MR <10962717+Assasquid@users.noreply.github.com>
2022-01-26 00:21:27 +0000

    Added tooltips for player controls in the inspector

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -6,27 +6,41 @@ using UnityEngine.InputSystem;
 public class PlayerControls : MonoBehaviour
 {
     [Header("Speed")]
+    [Tooltip("How fast the ship moves on the screen.")]
     [SerializeField] float throwSpeed = 19f;
     
     [Header("Ship Range")]
-    [SerializeField] float xRange = 50f;
+    [Tooltip("How far the ship can move on screen from left to right.")]
+    [SerializeField] float xRange = 10f;
+    [Tooltip("How far down the ship can move on screen.")]
     [SerializeField] float yTopRange = 4f;
+    [Tooltip("How far up the ship can move on screen.")]
     [SerializeField] float yBottomRange = -7f;
 
     [Header("Ship Rotation : Pitch")]
+    [Tooltip("Factor adapting the pitch (rotation on the x axis) " 
+        + "of the ship to the position on screen.")]    
     [SerializeField] float positionPitchFactor = -4f;
+    [Tooltip("Maximum rotation the ship can have at the top of the screen.")]
     [SerializeField] float maxTopPitch = -20f;
+    [Tooltip("Maximum rotation the ship can have at the bottom of the screen.")]
     [SerializeField] float maxBottomPitch = 30f;
+    [Tooltip("Pitch due to a control input.")]
     [SerializeField] float controlPitchFactor = -15f;
 
     [Header("Ship Rotation : Yaw")]
+    [Tooltip("Factor adapting the pitch (rotation on the y axis) " 
+        + "of the ship to the position on screen.")] 
     [SerializeField] float positionYawFactor = 3.9f;
+    [Tooltip("Yaw due to a control input.")]
     [SerializeField] float controlYawFactor = 19f;
 
     [Header("Shop Rotation : Roll")]
+    [Tooltip("Roll due to a control input. Roll is the ship's rotation on the z axis.")]
     [SerializeField] float controlRollFactor = -37f;
 
     [Header("Canons")]
+    [Tooltip("Particle System for each canon.")]
     [SerializeField] GameObject[] lasers;
 
     Vector2 moveInput;
