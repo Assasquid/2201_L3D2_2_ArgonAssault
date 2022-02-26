80c7f9bb5fa15e07c69763d0853235aa07d47c4b
Jerome MR <10962717+Assasquid@users.noreply.github.com>
2022-02-26 21:17:10 +0000

    Added score to the game

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,12 +6,31 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject deathVFX;
     [SerializeField] Transform parent;
+    [SerializeField] int pointValue;
 
-    void OnParticleCollision(GameObject other) 
+    ScoreBoard scoreBoard;
+
+    void Start()
+    {
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        ProcessHit();
+        DestroyEnemy();
+    }
+
+    void ProcessHit()
+    {
+        scoreBoard.IncreaseScore(pointValue);
+    }
+
+    void DestroyEnemy()
     {
         GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
         vfx.transform.parent = parent;
 
-        Destroy(gameObject);    
+        Destroy(gameObject);
     }
 }
